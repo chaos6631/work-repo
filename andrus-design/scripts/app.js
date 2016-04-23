f5d14d6f9dbbc746fc867562b294079312fd2608
chaos6631 <chriscalder@rogers.com>
2016-04-23 10:29:32 -0400

    // created routes and templates

--- andrus-design/scripts/app.js
+++ andrus-design/scripts/app.js
@@ -1,4 +1,64 @@
-angular
-	.module('app', [
-		'ui.bootstrap'
-	])
+// Create the module and name it app
+var app = angular.module('app', ['ngRoute']);
+
+
+
+
+
+app.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider.otherwise("/");
+			$routeProvider.				
+				when('/', {					
+					templateUrl: "templates/home.html",
+					controller: "homeController"
+				}).					
+				when('/about', {
+					templateUrl: "templates/about.html",
+					controller: "aboutController"
+				}).				
+				when('/contact', {					
+					templateUrl: "templates/contact.html",
+					controller: "contactController"
+				}).
+				when('/news', {					
+					templateUrl: "templates/news.html",
+					controller: "newsController"
+				}).
+				when('/projects', {					
+					templateUrl: "templates/projects.html",
+					controller: "projectsController"
+				}).
+				when('/team', {					
+					templateUrl: "templates/team.html",
+					controller: "teamController"
+				});
+	}]);
+
+app.controller('homeController', function($scope){
+	$scope.hello = "Hello I am Working!";
+});
+
+app.controller('aboutController', function($scope){
+	$scope.hello = "Hello I am Working!";
+});
+
+app.controller('contactController', function($scope){
+	$scope.hello = "Hello I am Working!";
+});
+
+app.controller('newsController', function($scope){
+	$scope.hello = "Hello I am Working!";
+});
+
+app.controller('projectsController', function($scope){
+	$scope.hello = "Hello I am Working!";
+});
+
+app.controller('teamController', function($scope){
+	$scope.hello = "Hello I am Working!";
+});
+// Create the home page controller
+// .controller('homeController', function ($scope) {
+// 	$scope.message = "Home Page Test Message";
+// });
--- andrus-design/scripts/controllers/homeCtrl.js
+++
@@ -1,5 +0,0 @@
-angular
-	.module('app')
-	.controller('appController', function($scope){
-		// $scope.data /*This is a data conatiner*/
-	})
