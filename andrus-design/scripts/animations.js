b3bb19064a08de3222fa285517ecd79230bdc1ef
chaos6631 <chriscalder@rogers.com>
2016-04-30 14:25:18 -0400

    added responsive image scaling to homepage carousel

--- andrus-design/scripts/animations.js
+++ andrus-design/scripts/animations.js
@@ -1,40 +1,6 @@
 angular.module('appAnimations', ['ngAnimate']);
 
 /****************************************
-			Element Fade In on Scroll
-****************************************/
-/***** Reference: http://www.sitepoint.com/scroll-based-animations-jquery-css3/ ******/
-// $(function(){
-// 	// Cache a reference to window and animation items
-// 	var $animationElements = $('.scroll-animate');
-// 	var $window = $(window);
-// 	function CheckView() {
-// 	  var window_height = $window.height();
-// 	  var window_top_position = $window.scrollTop();
-// 	  var window_bottom_position = (window_top_position + window_height);
-
-// 	  $.each($animationElements, function() {
-// 	    var $element = $(this);
-// 	    var element_height = $element.outerHeight();
-// 	    var element_top_position = $element.offset().top;
-// 	    var element_bottom_position = (element_top_position + element_height);
-
-// 	    //check to see if this current container is within viewport
-// 	    if ((element_bottom_position >= window_top_position) &&
-// 	        (element_top_position <= window_bottom_position)) {
-// 	      $element.addClass('in-view');
-// 	    } else {
-// 	      $element.removeClass('in-view');
-// 	    }
-// 	  });
-// 	}
-// 	// Listen for scroll and window resize events
-// 	$window.on('scroll resize', CheckView());
-// 	// Trigger a scroll event on initial load so that items within view are loaded 
-// 	$window.trigger('scroll');
-// });
-
-/****************************************
 			Toggle Nav
 ****************************************/
 /* Open when someone clicks on the Nav button */
@@ -55,9 +21,21 @@ $(document).on('click','.navbar-collapse.in',function(e) {
 });
 
 /****************************************
-			Cycle Images
+			adjust image height based on window size for small desktop windows
 ****************************************/
-$('.carousel').carousel({
-	interval: 10000,	
-});
 
+$(window).resize(function(){
+
+	var imgHeight = 100;
+	var $winHeight = $(window).height();
+	var $img = $('.item-image-container > img');
+
+	if ($winHeight < 542){
+		imgHeight = imgHeight + ((($winHeight / 542) * -100) +105);
+		imgHeight = imgHeight + "vh";
+		$img.css("max-height", imgHeight);
+	}else{
+		$img.css("max-height", "100vh");
+	}
+
+});
